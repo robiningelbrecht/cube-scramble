242631c0a9f47ea7540270ee302404e64e747a78
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-06-06 09:20:57 +0200

    More tests

--- tests/RandomScrambleTest.php
+++ tests/RandomScrambleTest.php
@@ -19,35 +19,41 @@ class RandomScrambleTest extends TestCase
             $scramble,
             CubeScramble::fromNotation($scramble, Size::fromInt(2)),
         );
+        $this->assertCount(9, explode(' ', (string) $scramble));
 
         $scramble = RandomScramble::threeByThree();
         $this->assertEquals(
             $scramble,
             CubeScramble::fromNotation($scramble, Size::fromInt(3)),
         );
+        $this->assertCount(20, explode(' ', (string) $scramble));
 
         $scramble = RandomScramble::fourByFour();
         $this->assertEquals(
             $scramble,
             CubeScramble::fromNotation($scramble, Size::fromInt(4)),
         );
+        $this->assertCount(44, explode(' ', (string) $scramble));
 
         $scramble = RandomScramble::fiveByFive();
         $this->assertEquals(
             $scramble,
             CubeScramble::fromNotation($scramble, Size::fromInt(5)),
         );
+        $this->assertCount(60, explode(' ', (string) $scramble));
 
         $scramble = RandomScramble::sixBySix();
         $this->assertEquals(
             $scramble,
             CubeScramble::fromNotation($scramble, Size::fromInt(6)),
         );
+        $this->assertCount(80, explode(' ', (string) $scramble));
 
         $scramble = RandomScramble::sevenBySeven();
         $this->assertEquals(
             $scramble,
             CubeScramble::fromNotation($scramble, Size::fromInt(7)),
         );
+        $this->assertCount(100, explode(' ', (string) $scramble));
     }
 }
